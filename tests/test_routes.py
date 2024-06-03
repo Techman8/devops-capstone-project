ff29cc88bc2af41e096d8226c111d9842645086d
Techman8 <109234830+Techman8@users.noreply.github.com>
2024-06-03 12:09:25 -0700

    Update test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -39,7 +39,6 @@ class TestAccountService(TestCase):
         app.logger.setLevel(logging.CRITICAL)
         init_db(app)
 
-  
     @classmethod
     def tearDownClass(cls):
         """Runs once before test suite"""
@@ -52,7 +51,6 @@ class TestAccountService(TestCase):
 
         self.client = app.test_client()
 
-  
     def tearDown(self):
         """Runs once after each test case"""
         db.session.remove()
@@ -61,7 +59,6 @@ class TestAccountService(TestCase):
     #  H E L P E R   M E T H O D S
     ######################################################################
 
-  
     def _create_accounts(self, count):
         """Factory method to create accounts in bulk"""
         accounts = []
@@ -82,13 +79,11 @@ class TestAccountService(TestCase):
     #  A C C O U N T   T E S T   C A S E S
     ######################################################################
 
-  
     def test_index(self):
         """It should get 200_OK from the Home Page"""
         response = self.client.get("/")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-  
     def test_health(self):
         """It should be healthy"""
         resp = self.client.get("/health")
@@ -96,7 +91,6 @@ class TestAccountService(TestCase):
         data = resp.get_json()
         self.assertEqual(data["status"], "OK")
 
-  
     def test_create_account(self):
         """It should Create a new Account"""
         account = AccountFactory()
@@ -119,13 +113,11 @@ class TestAccountService(TestCase):
         self.assertEqual(new_account["phone_number"], account.phone_number)
         self.assertEqual(new_account["date_joined"], str(account.date_joined))
 
-  
     def test_bad_request(self):
         """It should not Create an Account when sending the wrong data"""
         response = self.client.post(BASE_URL, json={"name": "not enough data"})
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
-  
     def test_unsupported_media_type(self):
         """It should not Create an Account when sending the wrong media type"""
         account = AccountFactory()
@@ -136,7 +128,6 @@ class TestAccountService(TestCase):
         )
         self.assertEqual(response.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
 
-  
     def test_get_account(self):
         """It should Read a single Account"""
         account = self._create_accounts(1)[0]
@@ -147,19 +138,16 @@ class TestAccountService(TestCase):
         data = resp.get_json()
         self.assertEqual(data["name"], account.name)
 
-  
     def test_get_account_not_found(self):
         """It should not Read an Account that is not found"""
         resp = self.client.get(f"{BASE_URL}/0")
         self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
 
-  
     def test_405(self):
         """It should not allow the post method"""
         response1 = self.client.post()
         self.assertEqual(response1.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
 
-  
     def test_get_account_list(self):
         """It should Get a list of Accounts"""
         self._create_accounts(5)
@@ -168,7 +156,6 @@ class TestAccountService(TestCase):
         data = resp.get_json()
         self.assertEqual(len(data), 5)
 
-  
     def test_update_account(self):
         """It should Update an existing Account"""
 
@@ -186,14 +173,12 @@ class TestAccountService(TestCase):
 
         self.assertEqual(updated_account["name"], "Something Known")
 
-  
     def test_delete_account(self):
         """It should Delete an Account"""
         account = self._create_accounts(1)[0]
         resp = self.client.delete(f"{BASE_URL}/{account.id}")
         self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
 
-  
     def test_security_headers(self):
         """It should return security headers"""
         response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
